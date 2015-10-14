3956dcf3ea3e15a8569ba60bb8e10fd4562b98bb
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2015-10-14 22:43:33 +0700

    flash lufa instead of pjrc

--- keyboard/hid_liber/flash.sh
+++ keyboard/hid_liber/flash.sh
@@ -3,5 +3,5 @@
 echo place magnet near controller now
 sleep 10
 sudo dfu-programmer atmega32u4 erase
-sudo dfu-programmer atmega32u4 flash hid_liber_pjrc.hex
+sudo dfu-programmer atmega32u4 flash hid_liber_lufa.hex
 sudo dfu-programmer atmega32u4 start
