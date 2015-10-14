101db0db88e7dc228f631fdf7c7321a18fbcaaa7
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2015-10-14 15:27:56 +0700

    right cursor & insert key

--- keyboard/hid_liber/keymap_priyadi.h
+++ keyboard/hid_liber/keymap_priyadi.h
@@ -15,7 +15,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------|     ,---.    
  * |Shft|iso|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |     |Up |    
  * |-----------------------------------------------------------| ,-----------.
- * |Ctl|Gui|Alt|          Space/Fn             |Alt|Gui|App|Ctl| |Lef|Dow|Rig|
+ * |Ctl|Gui|Alt|          Space/Fn0            |Fn1|Alt|Gui|Ctl| |Lef|Dow|Rig|
  * `-----------------------------------------------------------' `-----------'
  */
 
@@ -25,9 +25,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TAB,    Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P, LBRC, RBRC, BSLS,  DEL,  END, PGDN, \
      CAPS,    A,    S,    D,    F,    G,    H,    J,    K,    L, SCLN, QUOT,        ENT,                   \
      LSFT, NUBS,    Z,    X,    C,    V,    B,    N,    M, COMM,  DOT, SLSH,       RSFT,         UP,       \
-     LCTL, LGUI, LALT,              FN0,                         RALT, RGUI,  APP, RCTL, LEFT, DOWN, RGHT),
+     LCTL, LGUI, LALT,              FN0,                          FN1, RALT, RGUI, RCTL, LEFT, DOWN, RGHT),
 
-/* Layer 1:
+/* Layer 1: SpaceFn layer
  *
  * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
  * |   |   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |
@@ -41,7 +41,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------|     ,---.    
  * |    |   |   |   |   |   |Spc|End|   |   |   |   |          |     |   |    
  * |-----------------------------------------------------------| ,-----------.
- * |   |   |   |                               |   |   |   |   | |   |   |   |
+ * |   |   |   |               ***             |   |   |App|Ins| |   |   |   |
  * `-----------------------------------------------------------' `-----------'
  */
 
@@ -51,10 +51,36 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      TRNS, TRNS, MPRV, MPLY, MNXT, TRNS, TRNS, PGUP,   UP, PGDN, PSCR, VOLD, VOLU, MUTE, TRNS, TRNS, TRNS, \
      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, LEFT, DOWN, RGHT, TRNS, TRNS,       TRNS,                   \
      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  SPC,  END, TRNS, TRNS, TRNS, TRNS,       TRNS,       TRNS,       \
-     TRNS, TRNS, TRNS,                   TRNS,                   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS),
+     TRNS, TRNS, TRNS,                   TRNS,                     NO, TRNS,  APP,  INS, TRNS, TRNS, TRNS),
 
+/* Layer 2: right cursor keys
+ *
+ * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
+ * |   |   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |
+ * `---'   `---------------' `---------------' `---------------' `-----------'
+ * ,-----------------------------------------------------------. ,-----------.
+ * |   |   |   |   |   |   |   |   |   |   |   |   |   |       | |   |   |   |
+ * |-----------------------------------------------------------| |-----------|
+ * |     |   |   |   |   |   |   |   |   |   |   |   |   |     | |   |   |   |
+ * |-----------------------------------------------------------| `-----------'
+ * |      |   |   |   |   |   |   |   |   |   |   |Pgu|        |              
+ * |-----------------------------------------------------------|     ,---.    
+ * |    |   |   |   |   |   |   |   |   |Hom|End|PgD|   Up     |     |   |    
+ * |-----------------------------------------------------------| ,-----------.
+ * |   |   |   |                               |***|Lef|Dow|Rig| |   |   |   |
+ * `-----------------------------------------------------------' `-----------'
+ */
+
+ KEYMAP(\
+       NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,         NO,   NO,   NO, \
+       NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO, \
+       NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO, \
+       NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO, PGUP,         NO,                   \
+       NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO, HOME,  END, PGDN,         UP,         NO,       \
+     TRNS, TRNS, TRNS,                     NO,                     NO, LEFT, DOWN, RGHT,   NO,   NO,   NO),
 };
 
 static const uint16_t PROGMEM fn_actions[] = {
-	ACTION_LAYER_TAP_KEY(1, KC_SPC)
+	[0] = ACTION_LAYER_TAP_KEY(1, KC_SPC),
+  [1] = ACTION_LAYER_MOMENTARY(2)
 };
